7a1ad1cca4bd2f4ac3eafb9e07c1ea7c0dc667d2
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2020-10-15 02:34:09 +0900

    SecondRelease

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -4,7 +4,24 @@ import TourIntro from './tourIntro';
 import { Link } from 'react-router-dom';
 
 class main extends Component{
+  state = {
+    answer : ''
+  }
 
+  async componentDidMount(){
+    const response = await fetch('/answer/stage',{
+      method : 'POST',
+      headers : {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    const body = await response.text();
+
+    this.setState({
+      answer : body
+    })
+  }
   // 중간에 "클릭" 버튼 눌렀을 때 /api/insert 로 라우팅해줘서 MongoDB와 연결
   onClickButton = async e => {
     const response = await fetch('/api/insert', {
@@ -19,8 +36,11 @@ class main extends Component{
 
     // this.setState({responseToPost: body});
   }
-
+  goSuccess = () => {
+    
+  }
   render(){
+    console.log(this.state);
     return (
       <div>
             {/* <!-- 컨텐츠 부분 --> */}
@@ -40,8 +60,8 @@ class main extends Component{
 
             {/* <p>퀴즈의 정답을 입력해주세요</p> */}
             <input className="submit_input" type="text" />
-            <button id="quiz_button" className="submit_button">확인</button>
-        </div>
+            <Link to='/quiz'><button id="quiz_button" className="submit_button">확인</button></Link>
+        </div> 
 
         <hr />
 
@@ -50,7 +70,7 @@ class main extends Component{
             <strong>QR코드를 찾아 문제를 해결하고 힌트를 모아, 4자리 비밀번호를 찾으세요. 비밀번호를 찾으셨다면 아래 입력창에 입력하세요.</strong>
             <br />
             <input className="submit_input" type="text" />
-            <button id="mission_button" className="submit_button">확인</button>
+            <button id="mission_button" className="submit_button">제출</button>
         </div>
 
         {/* 몽고 DB 테스트 하는 곳 */}
